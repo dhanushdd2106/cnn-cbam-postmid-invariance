a9bc4f8130c764239b5d39f700ff7019c174af1e
Dhanush Doddapaneni <dhanushdoddapaneni21@gmail.com>
2026-07-27 21:09:11 +0530

    changed robustness  for cifar 10

--- evaluation/evaluate_robustness_cnn_cbam.py
+++ evaluation/evaluate_robustness_cnn_cbam.py
@@ -159,19 +159,19 @@ def get_dataset(dataset_name, extra_transform):
 
         base_transform = transforms.Compose([
 
-            extra_transform,
+        transforms.ToTensor(),
 
-            transforms.ToTensor(),
+        extra_transform,
 
-            transforms.Normalize(
+        transforms.Normalize(
 
-                (0.4914, 0.4822, 0.4465),
+            (0.4914,0.4822,0.4465),
 
-                (0.2470, 0.2435, 0.2616)
+            (0.2470,0.2435,0.2616)
 
-            )
+        )
 
-        ])
+    ])
 
 
 
